349ef551591f2a3795e49da7d3dbcbb442d50639
dat <dat@gamil.com>
2024-04-02 21:27:56 +0700

    add cors

--- node-be/mini-project/comment/index.js
+++ node-be/mini-project/comment/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import { randomBytes } from "crypto";
 import bodyParser from "body-parser";
 import { log } from "console";
+import cors from "cors";
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const postCommentIds = {};
 
